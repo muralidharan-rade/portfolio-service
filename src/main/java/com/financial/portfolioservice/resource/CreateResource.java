175f612723863440846aef44db57291d3caf024e
muralidharan-rade <muralidharan.balanandan@walmart.com>
2022-07-25 23:01:44 +0530

    health check probe

--- src/main/java/com/financial/portfolioservice/resource/CreateResource.java
+++ src/main/java/com/financial/portfolioservice/resource/CreateResource.java
@@ -3,12 +3,17 @@ package com.financial.portfolioservice.resource;
 import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.financial.portfolioservice.beans.Portfolio;
 import com.financial.portfolioservice.service.AllocationService;
+
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.Map;
 
 @RestController
 public class CreateResource {
@@ -20,6 +25,12 @@ public class CreateResource {
 	public Portfolio createPortfolio(@RequestBody @Valid Portfolio request) {
 
 		return allocationService.allocatePortfolio(request);
+	}
+
+	@GetMapping(produces = "application/json")
+	public Map<String, String> sampleTest() {
+
+		return Collections.singletonMap("status", "healthy");
 	}
 
 }
